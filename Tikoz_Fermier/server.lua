ac69daa04062eaaf85671607c22f5a134ca45d0e
Tikoz <106456046+Tikozaal@users.noreply.github.com>
2022-09-19 17:16:29 +0200

    Update server.lua

--- Tikoz_Fermier/server.lua
+++ Tikoz_Fermier/server.lua
@@ -357,7 +357,7 @@ AddEventHandler("Tikoz:fermierdepotentreprise", function(money)
     local _source = source
     local xPlayer = ESX.GetPlayerFromId(_source)
     local total = money
-    local xMoney = xPlayer.getMoney()
+    local xMoney = xPlayer.getAccount("bank").money
     
     TriggerEvent('esx_addonaccount:getSharedAccount', "society_fermier", function (account)
         if xMoney >= total then
